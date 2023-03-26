bb89720e1763b3ada41e1b2d89f86e51ea2d80c2
Harrison White <p2596843@my365.dmu.ac.uk>
2023-03-26 12:34:35 +0100

    Week 25 Requests Complete

--- AdminSystem/6RequestsList.aspx
+++ AdminSystem/6RequestsList.aspx
@@ -10,6 +10,7 @@
     <form id="form1" runat="server">
         <div>
         </div>
+        <asp:ListBox ID="lstRequestList" runat="server" Height="667px" OnSelectedIndexChanged="lstRequestList_SelectedIndexChanged" Width="638px"></asp:ListBox>
     </form>
 </body>
 </html>
---
+++ ClassLibrary/clsRequestsCollection.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class clsRequestsCollection
+    {
+        List<clsRequests> mRequestList = new List<clsRequests>();
+
+        public clsRequestsCollection()
+        {
+            Int32 Index = 0;
+            Int32 RecordCount = 0;
+
+            clsDataConnection DB = new clsDataConnection();
+
+            DB.Execute("sproc_tblRequests_SelectAll");
+            RecordCount = DB.Count;
+
+            while (Index < RecordCount)
+            {
+                clsRequests AnRequest = new clsRequests();
+
+                AnRequest.requestID = Convert.ToInt32(DB.DataTable.Rows[Index]["requestID"]);
+                AnRequest.postcode = Convert.ToString(DB.DataTable.Rows[Index]["postcode"]);
+                AnRequest.flumeCount = Convert.ToInt32(DB.DataTable.Rows[Index]["flumeCount"]);
+
+                mRequestList.Add(AnRequest);
+
+                Index++;
+            }
+
+
+        }
+
+
+        public List<clsRequests> RequestList
+        {
+            get
+            {
+                return mRequestList;
+
+            }
+            set
+            {
+                mRequestList = value;
+            }
+        }
+
+        public clsRequests ThisRequest { get; set; }
+        public int Count
+        {
+            get
+            {
+                return mRequestList.Count;
+            }
+            set
+            {
+                //worry ab this later
+            }
+        }
+    }
+}
